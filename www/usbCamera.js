da36e58ddab8bd70919d47b3a1faa16f8e253ba6
Alessandro-Puglisi21 <alessandropuglisi2000@gmail.com>
2025-08-05 16:20:48 +0200

    startPewview

--- www/usbCamera.js
+++ www/usbCamera.js
@@ -1,58 +1,26 @@
-var exec = require('cordova/exec');
-
 var UsbCamera = {
-    /**
-     * Open external USB camera
-     * @param {Object} options - Camera options {width, height, fps}
-     * @param {Function} onFrame - Callback for each frame (base64 string)
-     * @param {Function} onError - Error callback
-     */
-    open: function(options, onFrame, onError) {
-        options = options || {};
-        
-        var success = function(result) {
-            if (typeof onFrame === 'function') {
-                onFrame(result);
-            }
-        };
-        
-        var error = function(err) {
-            if (typeof onError === 'function') {
-                onError(err);
-            }
-        };
-        
-        exec(success, error, 'UsbExternalCamera', 'open', [options]);
+    open: function(options, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, 'UsbExternalCamera', 'open', [options]);
     },
     
-    /**
-     * Stop camera preview
-     * @param {Function} callback - Success callback
-     * @param {Function} errorCallback - Error callback
-     */
-    stopPreview: function(callback, errorCallback) {
-        exec(callback, errorCallback, 'UsbExternalCamera', 'stopPreview', []);
+    startPreview: function(successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, 'UsbExternalCamera', 'startPreview', []);
     },
     
-    /**
-     * Take a photo and save to device storage
-     * @param {Function} callback - Success callback with file path
-     * @param {Function} errorCallback - Error callback
-     */
-    takePhoto: function(callback, errorCallback) {
-        exec(callback, errorCallback, 'UsbExternalCamera', 'takePhoto', []);
+    stopPreview: function(successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, 'UsbExternalCamera', 'stopPreview', []);
     },
     
-    /**
-     * Close camera and release resources
-     * @param {Function} callback - Success callback
-     * @param {Function} errorCallback - Error callback
-     */
-    close: function(callback, errorCallback) {
-        exec(callback, errorCallback, 'UsbExternalCamera', 'close', []);
+    takePhoto: function(successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, 'UsbExternalCamera', 'takePhoto', []);
     },
-    listCameras: function (callback, errorCallback) {
-        exec(callback, errorCallback, 'UsbExternalCamera', 'listCameras', []);
+    
+    close: function(successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, 'UsbExternalCamera', 'close', []);
+    },
+    
+    listCameras: function(successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, 'UsbExternalCamera', 'listCameras', []);
     }
 };
 
